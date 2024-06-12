9a7852cd8f357ccdc92619265a07baab5ba3c25c
xonix70 <xonix@mail.ru>
2024-06-12 21:41:02 +0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,22 @@
+# длина слова
+a=print(len('хороший мальчик'))
+#«Суммы и разности»
+first = 8
+second=11
+summa=print(first+second)
+diff=print(first-second)
+#«Среднее арифметическое»
+first=14
+second=7
+third=21
+mean=print((first+second+third)/3)
+#Простые строчки
+first_string='Вторник'
+second_string='Понедельник'
+print(second_string+', '+first_string)
+#Сложная формула
+a=7
+b=5
+c=9
+f=(a*b)+(a*c)
+print((f**3)/2)
